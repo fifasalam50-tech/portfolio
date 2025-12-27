3938aa42f0014ad050822161720d210c5ee59553
fifasalam50-tech <fifasalam50@gmail.com>
2025-12-27 18:51:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const text = document.getElementById("dreamText");
 btn.addEventListener("click", () => {
   text.classList.toggle("hidden");
 });
+
+document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    const href = this.getAttribute("href");
+
+    if (!href || href.startsWith("http")) return;
+
+    e.preventDefault();
+    document.body.classList.add("fade-out");
+
+    setTimeout(() => {
+      window.location.href = href;
+    }, 400);
+  });
+});
+
+window.addEventListener("load", () => {
+  document.body.classList.remove("fade-out");
+});
